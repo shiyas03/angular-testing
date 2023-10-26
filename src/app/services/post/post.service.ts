5bd0eeb491f8cc4e820cc4c8ad23b9b1195aeb3d
shiyas03 <shiyastofficial@gmail.com>
2023-10-26 19:09:49 +0530

    pipe and services isolate testing done

---
+++ src/app/services/post/post.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from 'src/app/models/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  
+  constructor(private http:HttpClient) { }
+
+  getPosts(){
+    return this.http.get<Post[]>(`http://jsonplaceholder.typicode.com/posts`)
+  }
+
+  deletePost(post:Post){
+    return this.http.delete(`http://jsonplaceholder.typicode.com/post/${post.id}`)
+  }
+}
